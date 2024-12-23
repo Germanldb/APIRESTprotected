c9a139dcfb1a5fcab4d95d4549dbd7929732ea68
German Devia <148661587+Germanldb@users.noreply.github.com>
2024-12-23 11:00:24 -0400

    Update APIREST-Protected.php

--- APIREST-Protected.php
+++ APIREST-Protected.php
@@ -5,6 +5,12 @@ add_filter( 'rest_authentication_errors', function( $result ) {
     if ( ! is_user_logged_in() ) {
         return new WP_Error( 'rest_not_logged_in', 'You are not authorized to access the REST API', 401 );
     }
+
+    $user = wp_get_current_user();
+    if ( ! in_array( 'administrator', (array) $user->roles ) ) {
+        return new WP_Error( 'rest_forbidden', 'You do not have permissions to access the REST API', 403 );
+    }
+
     return $result;
 });
 
